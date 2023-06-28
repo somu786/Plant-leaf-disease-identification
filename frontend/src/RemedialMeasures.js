1c530316246f3f4b729233266fc4c3d69ab4699e
Somasundaram S <71077386+somu786@users.noreply.github.com>
2023-06-28 07:55:47 +0530

    first commit

---
+++ frontend/src/RemedialMeasures.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function RemedialMeasures() {
+  return (
+    <div>
+      <h1>Remedial Measures for Potato Plant Diseases</h1>
+  
+  <h2>Early Blight Disease</h2>
+  <p>Early blight disease is a common potato plant disease that affects the leaves and stems of the plant. Here are some remedial measures to prevent and control early blight disease:</p>
+  <ul>
+    <li>Remove and destroy infected leaves and stems as soon as possible to prevent the spread of the disease.</li>
+    <li>Avoid overhead watering, as the splashing of water can spread the disease to healthy plants.</li>
+    <li>Use a copper-based fungicide spray to control the spread of the disease.</li>
+    <li>Rotate crops every year to prevent the buildup of disease-causing organisms in the soil.</li>
+  </ul>
+  
+  <h2>Late Blight Disease</h2>
+  <p>Late blight disease is a serious potato plant disease that can cause significant yield losses. Here are some remedial measures to prevent and control late blight disease:</p>
+  <ul>
+    <li>Remove and destroy infected leaves, stems, and tubers as soon as possible to prevent the spread of the disease.</li>
+    <li>Apply fungicides containing chlorothalonil, mancozeb, or copper to control the spread of the disease.</li>
+    <li>Avoid planting potatoes in areas with high humidity and rainfall, as these conditions favor the development of the disease.</li>
+    <li>Use certified seed potatoes to avoid introducing the disease into your garden.</li>
+  </ul>
+    </div>
+  );
+}
+
+export default RemedialMeasures;
